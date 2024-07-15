134960c52fca30a4cbe9fa3aea96c9073a402305
bhaveshbalendra <balendrabhavesh@gmail.com>
2024-07-15 20:34:25 +0530

    Remove all files

--- 02-AddBoxAndRemove/index.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector(".btn");
-
-btn.addEventListener("click", () => {
-  const li = document.createElement("li");
-  li.classList.add("grid-box");
-  li.innerText = "Hello";
-  const ul = document.querySelector("ul");
-  ul.append(li);
-});
